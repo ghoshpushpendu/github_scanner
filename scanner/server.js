618081405922976ba636e124649cef5aded2c062
pushpendu <pushpendu.ghosh@apptware.com>
2023-11-23 17:37:37 +0530

    updated

--- scanner/server.js
+++ scanner/server.js
@@ -21,7 +21,7 @@ const typeDefs = gql`
     isPrivate: Boolean
     filesCount: Int
     ymlContent: String
-    activeWebhooks: Int
+    activeWebhooks: String
   }
 
   type Query {
@@ -33,15 +33,11 @@ const typeDefs = gql`
 const resolvers = {
     Query: {
         listRepositories: async () => {
-
-            const {
-                data: { login },
-            } = await octokit.rest.users.getAuthenticated();
-
-            console.log('login', login);
-
             // Fetch list of repositories from GitHub
-            const { data } = await octokit.rest.repos.listForAuthenticatedUser();
+            const { data } = await octokit.rest.repos.listForUser({
+                username: 'ghoshpushpendu',
+                per_page: 1000,
+            });
             return data.map(repo => ({
                 name: repo.name,
                 size: repo.size,
@@ -56,7 +52,8 @@ const resolvers = {
             });
 
             const filesCount = await getFileCount(repo.owner.login, repo.name);
-            console.log('filesCount', filesCount);
+            const webhooks = await getActiveWebhooks(repo.owner.login, repo.name);
+            const ymlContent = await getContentOfYamlFiles(repo.owner.login, repo.name);
 
             // Additional logic to get details like private/public, number of files, content of 1 YAML file, active webhooks, etc.
             // ...
@@ -66,7 +63,9 @@ const resolvers = {
                 size: repo.size,
                 owner: repo.owner.login,
                 isPrivate: repo.private,
-                filesCount: filesCount
+                filesCount: filesCount,
+                activeWebhooks: webhooks,
+                ymlContent: ymlContent,
                 // Add other details as needed
             };
         },
@@ -86,6 +85,61 @@ async function getFileCount(owner, repo) {
         return files.length;
     } catch (error) {
         console.error('Error fetching repository content:', error.message);
+    }
+}
+
+async function getActiveWebhooks(owner, repo) {
+    try {
+        // Get the list of webhooks for the repository
+        const response = await octokit.rest.repos.listWebhooks({
+            owner: owner,
+            repo: repo,
+        });
+
+        const webhooks = response.data.map(webhook => ({
+            id: webhook.id,
+            name: webhook.name,
+            events: webhook.events,
+            config: webhook.config,
+            active: webhook.active,
+        }));
+
+        return JSON.stringify(webhooks, null, 2);
+    } catch (error) {
+        console.error('Error fetching webhooks:', error.message);
+    }
+}
+
+async function getContentOfYamlFiles(owner, repo) {
+    try {
+        // Get the list of files in the repository
+        const response = await octokit.rest.repos.getContent({
+            owner: owner,
+            repo: repo,
+            path: '', // Path to the root directory
+        });
+
+        // Filter for YAML files
+        const yamlFiles = response.data.filter(file => file.name.endsWith('.yaml') || file.name.endsWith('.yml'));
+
+        if (yamlFiles.length === 0) {
+            return 'No YAML files found';
+        }
+        console.log('YAML files:', yamlFiles);
+        // Fetch and log the content of each YAML file
+        const contentResponse = await octokit.rest.repos.getContent({
+            owner: owner,
+            repo: repo,
+            path: yamlFiles[0].path,
+        });
+
+        // Decode the content from base64
+        const content = Buffer.from(contentResponse.data.content, 'base64').toString('utf-8');
+        console.log('Content of YAML file:', content);
+        return content;
+
+    } catch (error) {
+        console.error('Error fetching YAML files:', error.message);
     }
 }
 
